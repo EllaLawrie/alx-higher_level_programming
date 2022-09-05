59f13e1f08d13eb5dd83c7b30f4d6bdf6dad5eaf
EllaLawrie <ellalawrie002@gmail.com>
2022-09-05 13:15:47 -0700

    Js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,5 @@
 // ['C is fun', 'Python is cool', 'JavaScript is amazing'].forEach((e) => console.log(e));
 
 const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const msgs in msg) {
-	console.log(msg[msgs]);
-}
+
+msg.forEach((element) => console.log(element));
